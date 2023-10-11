bca4701b1eab6dba33f30c531afcd44b3d69a48c
Jiangqi <jiangqisu@gmail.com>
2023-10-10 22:44:35 -0500

    added a header template

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import DefaultHeader from './Components/DefaultHeader';
 import ShortenerInput from './Components/shortenerInput';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <DefaultHeader></DefaultHeader>
       <ShortenerInput></ShortenerInput>
     </div>
   );
