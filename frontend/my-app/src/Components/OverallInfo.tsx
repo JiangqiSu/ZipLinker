5b419e8e8f9dca96bc9621231a990ee43695ecca
Lily2524 <yx2000911@163.com>
2023-11-07 02:53:50 -0600

    add filter and search to dashboard

--- frontend/my-app/src/Components/OverallInfo.tsx
+++ frontend/my-app/src/Components/OverallInfo.tsx
@@ -6,6 +6,10 @@ import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
 import React, { useState } from 'react';
+import {Typography} from "@mui/material";
+import RadioGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Radio from "@mui/material/Radio";
 
 
 export const OverallInfo=()=>{
@@ -23,6 +27,24 @@ export const OverallInfo=()=>{
         <div>
             <Grid container spacing={2}>
                 <Grid item xs={3}>
+                    <Typography variant="h6" gutterBottom align={"left"}>
+                        Filter
+                    </Typography>
+                    <Typography variant="subtitle1" gutterBottom align={"left"}>
+                        Time Span
+                    </Typography>
+                    <RadioGroup sx={{position: 'flex', flexDirection: 'row'}}>
+                        <FormControlLabel control={<Radio size="small"/>} label={<Typography variant="body2">Last week</Typography>} />
+                        <FormControlLabel control={<Radio size="small"/>} label={<Typography variant="body2">Last month</Typography>} />
+                        <FormControlLabel control={<Radio size="small"/>} label={<Typography variant="body2">Last year</Typography>} />
+                    </RadioGroup>
+                    <Typography variant="subtitle1" gutterBottom align={"left"}>
+                        URL Status
+                    </Typography>
+                    <RadioGroup sx={{position: 'flex', flexDirection: 'row'}}>
+                        <FormControlLabel control={<Radio size="small"/>} label={<Typography variant="body2">Active</Typography>} />
+                        <FormControlLabel control={<Radio size="small"/>} label={<Typography variant="body2">Expired</Typography>} />
+                    </RadioGroup>
                     <List sx={{marginLeft: '5%', marginRight: '10%'}} component="nav" aria-label="mailbox folders">
                         <ListItem button sx={{padding: '20px'}}
                                   onClick={(event) => handleListItemClick(event, 0)}>
