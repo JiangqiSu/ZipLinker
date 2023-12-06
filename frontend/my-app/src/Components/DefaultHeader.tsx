379488dd42578e1c1518cd04bd90e9d37a4c5579
Jiangqi <jiangqisu@gmail.com>
2023-12-06 17:43:10 -0600

    refine the link between home and management pages.

--- frontend/my-app/src/Components/DefaultHeader.tsx
+++ frontend/my-app/src/Components/DefaultHeader.tsx
@@ -1,23 +1,27 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import React, {useState, useEffect} from 'react';
 
 export default function DefaultHeader() {
+  const [manageOrHome, setManageOrHome] = useState(useLocation().pathname == '/management/:userId' ? 'Home' : 'Manage');
+
   const navigate = useNavigate();
   const handleLoginClick = () => {
     navigate('/login');
   };
   const handleManageClick = () => {
-    navigate('/management/:userId');
+    navigate(manageOrHome == 'Manage' ? '/management/:userId' : '/home');
   };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Button color="inherit" onClick={handleManageClick}>Manage</Button>
+          <Button color="inherit" onClick={handleManageClick}>{manageOrHome}</Button>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             ZipLinker
           </Typography>
