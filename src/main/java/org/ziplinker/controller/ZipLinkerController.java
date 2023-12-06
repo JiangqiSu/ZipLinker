3a65f6a14911dd514d092e132457e803b5967165
chengxuc <chengxuc@usc.edu>
2023-12-06 16:09:09 -0600

    added frontend setting

--- src/main/java/org/ziplinker/controller/ZipLinkerController.java
+++ src/main/java/org/ziplinker/controller/ZipLinkerController.java
@@ -25,6 +25,24 @@ public class ZipLinkerController {
             return;
         }
 
+        // Set up CORS
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
         /**
          * User Domain Endpoints
          */
